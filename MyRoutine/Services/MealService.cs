a59bba0b826c3be33eddec60c6ba67c5ffa21fd9
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-01-20 16:57:42 -0300

    Add new service MealService

---
+++ MyRoutine/Services/MealService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRoutine.Data;
+using MyRoutine.Models;
+using System.Linq;
+
+namespace MyRoutine.Services
+{
+    public class MealService
+    {
+        private readonly MyRoutineContext _context;
+        
+        public MealService(MyRoutineContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int?> SumCalories(int dietId)
+        {
+            return await _context.Meals.Where(x => x.DietId == dietId).SumAsync(x => x.Calories);
+        }
+        
+
+        
+    }
+}
