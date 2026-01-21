7f3386ecff8f1a51f789a300b8fe517782f5d5f2
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-01-21 12:51:21 -0300

    Update details and add new extensions

---
+++ MyRoutine/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System;
+using System.Linq;
+
+namespace MyRoutine.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            return value
+                .GetType()
+                .GetMember(value.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()?
+                .GetName()
+                ?? value.ToString();
+        }
+    }
+}
