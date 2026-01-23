6a869eb17cb8bca49b3232679d6ce06f4240f4da
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-01-23 11:50:07 -0300

    Add new methods to DailyDietService and create a new migration

---
+++ MyRoutine/Migrations/20260123144639_DailyDiet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyRoutine.Migrations
+{
+    /// <inheritdoc />
+    public partial class DailyDiet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailyDiets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DietId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyDiets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DailyDiets_Diets_DietId",
+                        column: x => x.DietId,
+                        principalTable: "Diets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DailyDiets_DietId",
+                table: "DailyDiets",
+                column: "DietId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyDiets");
+        }
+    }
+}
--- MyRoutine/Services/DailyDietService.cs
+++ MyRoutine/Services/DailyDietService.cs
@@ -1,4 +1,6 @@
-﻿using MyRoutine.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MyRoutine.Data;
+using MyRoutine.Models;
 
 namespace MyRoutine.Services
 {
@@ -11,6 +13,37 @@ namespace MyRoutine.Services
             _context = context;
         }
 
-        
+        public async Task<DailyDiet?> GetByDateAsync(DateTime date)
+        {
+            var today = date.Date;
+
+            return await _context.DailyDiets.Include(d => d.Diet).FirstOrDefaultAsync(d => d.Date == today);
+        }
+
+        public async Task<DailyDiet> SetDietForTodayAsync(int dietId)
+        {
+            var today = DateTime.Today;
+
+            var dailyDiet = await _context.DailyDiets.FirstOrDefaultAsync(x => x.Date == today);
+
+            if(dailyDiet == null)
+            {
+                dailyDiet = new DailyDiet
+                {
+                    DietId = dietId,
+                    Date = today
+                };
+                _context.DailyDiets.Add(dailyDiet);
+            }
+            else
+            {
+                dailyDiet.DietId = dietId;
+                _context.DailyDiets.Update(dailyDiet);
+            }
+            await _context.SaveChangesAsync();
+            return dailyDiet;
+
+
+        }
     }
 }
